1e518ce660f42a31bbfd7f33451d02a6511432b0
prieliot <eliot.hautefeuille@gmail.com>
2014-10-28 15:15:43 +0100

    Update sys.js
    Cleaned up addRow function, added Remise and auto row deletion

--- sys.js
+++ sys.js
@@ -1,88 +1,115 @@
 var num_elements;
 
 function addTableRow() {
-	var trow = document.createElement('tr');
-	trow.id = "trow_" + num_elements;
 
+	// Creates HTML Elements
+	var trow = document.createElement('tr');
 	var rcode = document.createElement('td');
 	var rlibelle = document.createElement('td');
 	var rprix = document.createElement('td');
 	var rquant = document.createElement('td');
 	var rmontant = document.createElement('td');
-
 	var slibelle = document.createElement('span');
 	var smontant = document.createElement('span');
 	var sprix = document.createElement('span');
+	var iquant = document.createElement('input');
+	var ccode = document.createElement('select');
 
+	// Attaches HTML id attributes
+	trow.id = "trow_" + num_elements;
 	slibelle.id = "libelle_" + num_elements;
 	smontant.id = "montant_" + num_elements;
 	sprix.id = "prix_" + num_elements;
+	iquant.id = "quantite_" + num_elements;
+	ccode.id = "code_" + num_elements;
+	
+	// Attaches other HTML attributes & attaches events
+	iquant.type = "text";
+	iquant.setAttribute("placeholder", "Quantité");
+	iquant.setAttribute("onchange", "updatePriceFor(" + num_elements + ")");
+	ccode.setAttribute("onchange", "updateDataFor(" + num_elements + ")");
 
+	// Adds content to HTML elements
 	slibelle.innerHTML = "-";
 	smontant.innerHTML = "0";
 	sprix.innerHTML = "-";
-
-	var iquant = document.createElement('input');
-	iquant.type = "text";
-	iquant.id = "quantite_" + num_elements;
-	iquant.setAttribute("placeholder", "Quantité");
-	iquant.setAttribute("onchange", "updatePriceFor(" + num_elements + ")");
-
+	
+	// Attaches HTML elements to #tableau, now they appear on the page
 	trow = document.getElementById("tableau").appendChild(trow);
 	rcode = trow.appendChild(rcode);
 	rlibelle = trow.appendChild(rlibelle);
 	rprix = trow.appendChild(rprix);
 	rquant = trow.appendChild(rquant);
 	rmontant = trow.appendChild(rmontant);
-
 	slibelle = rlibelle.appendChild(slibelle);
 	smontant = rmontant.appendChild(smontant);
 	sprix = rprix.appendChild(sprix);
-
 	iquant = rquant.appendChild(iquant);
-
-	var ccode = document.createElement('select');
-	ccode.id = "code_" + num_elements;
-	ccode.setAttribute("onchange", "updateDataFor(" + num_elements + ")");
-
 	ccode = rcode.appendChild(ccode);
-
+	
+	// Attaches options to the select list : adds code produit to the list
 	var tzero = document.createElement('option');
 	tzero.value = "zero";
 	tzero.innerHTML = "-";
-
 	tzero = ccode.appendChild(tzero);
-
 	for(var i = 0; i < data.length; i++) {
 		var txi = document.createElement('option');
 		txi.value = i;
 		txi.innerHTML = data[i][0];
 		txi = ccode.appendChild(txi);
 	}
-
+	
+	// Increments total number of rows in #tableau
 	num_elements++;
 }
 
-function updateRemise(ttc) {
-	// TODO
+function updateRowMgr() {
+	var i, x;
+	for(i = 0; i < num_elements; i++) {
+		x = document.getElementById("code_" + i).value;
+		if(x === "zero" && i < (num_elements - 1) && num_elements > 1)
+			return removeRow(i);
+	}
+	if(document.getElementById("code_" + (num_elements-1)).value !== "zero")
+		addTableRow();
 }
 
-function updateRowMgr() {
-	// alert("rowMgr");
+function removeRow(i) {
+	var bybye = document.getElementById("trow_" + i);
+	document.getElementById("tableau").removeChild(bybye);
+	for(var i = i + 1; i < num_elements; i++)
+	decrementRow(i);
+	num_elements--;
+}
 
-	// ESTIMATE SITUATION
-	var i, stupidityCount;
-	i = stupidityCount = 0;
-	for(i = 0; i < num_elements; i++) {
-		if(document.getElementById("code_" + i).value === "zero") stupidityCount++;
-	}
+function decrementRow(i) {
+	document.getElementById("trow_" + i).id = "trow_" + (i - 1);
+	document.getElementById("code_" + i).setAttribute("onchange", "updateDataFor(" + (i - 1) + ")");
+	document.getElementById("code_" + i).id = "code_" + (i - 1);
+	document.getElementById("libelle_" + i).id = "libelle_" + (i - 1);
+	document.getElementById("quantite_" + i).setAttribute("onchange", "updatePriceFor(" + (i - 1) + ")");
+	document.getElementById("quantite_" + i).id = "quantite_" + (i - 1);
+	document.getElementById("montant_" + i).id = "montant_" + (i - 1);
+	document.getElementById("prix_" + i).id = "prix_" + (i - 1);
+}
 
-	// SITUTATION ALPHA ALPHA - Simply add one more row, user is smart
-	if(stupidityCount == 0)
-		addTableRow();
-
-	// STILL TONS OF STUPIDITY CHECKS TO ENGAGE, UGH
-
+function updateRemise(ttc, sstt) {
+	if(ttc > 1000) { 
+		document.getElementById("remise").style.display = "block";
+		document.getElementById("remiseValue").innerHTML = (sstt * 0.05);
+		var nom = document.getElementById("nom_societe").value;
+		var tel = document.getElementById("tel_societe").value;
+		var email = nom + "@gmail.com";
+		if(nom != "" && tel != "") {
+			document.getElementById("discname").innerHTML = nom;
+			document.getElementById("disctel").innerHTML = tel;
+			document.getElementById("disaddr").innerHTML = email;
+		} else {
+			document.getElementById("discname").innerHTML = "[nomsociété]";
+			document.getElementById("disctel").innerHTML = "[telsociété]";
+			document.getElementById("disaddr").innerHTML = "[emailsociété]";
+		}
+	} else document.getElementById("remise").style.display = "none";
 }
 
 function updateTotals() {
@@ -94,9 +121,9 @@ function updateTotals() {
 
 	document.getElementById("soustotal").innerHTML = sstotal.toFixed(2);
 	document.getElementById("tva").innerHTML = (tva = sstotal * 0.196).toFixed(2);
-	document.getElementById("port").innerHTML = (port = 0).toFixed(2);		// QUERY PROF FOR THIS HUGE STUPIDITY
-	document.getElementById("ttc").innerHTML = ttc = (sstotal + tva + port).toFixed(2);
-	updateRemise(ttc);
+	document.getElementById("port").innerHTML = (port = sstotal * 0.1).toFixed(2);		// QUERY PROF FOR THIS HUGE STUPIDITY
+	document.getElementById("ttc").innerHTML = (ttc = (sstotal + tva + port)).toFixed(2);
+	updateRemise(ttc, sstotal);
 }
 
 function updateDataFor(i) {
@@ -117,7 +144,6 @@ function updateDataFor(i) {
 }
 
 function updatePriceFor(i) {
-	// alert("updatePrice");
 	p = Number(document.getElementById("prix_" + i).innerHTML);
 	p = p * Number(document.getElementById("quantite_" + i).value);
 	document.getElementById("montant_" + i).innerHTML = (isNaN(p)) ? 0 : p;
