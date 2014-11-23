1be4cf60b88b60a263008cacf090a0300f409ac4
prieliot <eliot.hautefeuille@gmail.com>
2014-11-23 22:41:20 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,15 @@
 /*	DATA - PRODUCTS */
 
 
+/* Tableau "donnees"
 
-var data = [
+Ce tableau contient toutes les informations de nos produits.
+Ceci permet d'éviter d'utiliser des switch partout et de
+faire moins d'erreurs dans notre code.
+->	[Code produit, Libéllé produit, Prix du produit]
+
+*/
+var donnees = [
 	["105 963 987", "Jimmy Cho", 1500],
 	["225 654 845", "UGG", 200],
 	["987 654 321","Yves Saint Laurent", 2000],
@@ -10,4 +17,21 @@ var data = [
 	["159 753 852", "Nike", 100],
 	["147 852 369", "Timberland", 200],
 	["584 586 987", "Gucci", 200]
+];
+
+/* Tableau "clients"
+
+Ce tableau contient toutes les informations de nos clients.
+Ceci permet d'éviter d'utiliser des switch partout et de
+faire moins d'erreurs dans notre code.
+->	[Nom client, Société, Addresse, Code Postal & Ville, Téléphone]
+
+*/
+var clients = [
+	["CARBONNE Olivier", "Carbonne and Co.", "12 Rue de la Pompe", "75016, Paris", "06.12.26.30.12"],
+	["COLONNA Alexis", "C'est une surprise", "6 Rue George Risler", "91390, Mosang-sur-Orge", "01.60.15.66.08"],
+	["HAUTEFEUILLE Eliot","HighLeaf", "11 Avenue Montaigne", "78600, Maisons-Laffitte", "06.74.23.18.96"],
+	["SERGEANT William", "TRANSPALETTE", "5 Rue du Général Leclerc", "87100, Limoges", "06.70.99.88.55"],
+	["PIONNEAU Céline", "C production", "62 Rue du Commerce", "75015, Paris", "06.23.28.03.11"],
+	["THOMASSINE Arthur", "Les minis", "6 Rue de la République", "24190, Chantérac", "06.55.69.86.87"]
 ];
